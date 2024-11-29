bf60520b90c27825cede915360b9261ac57c4932
takumi <tarakku32b3@gmail.com>
2024-11-29 12:33:01 +0900

    topページに外部cssを適用

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,9 +6,10 @@
     <title>ホーム</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
-<body>
 @include('navbar')
+<body class="hero-section">
     <main role="main" class="container" style="padding:60px 15px 0">
       <div>
         <!-- ここから「本文」-->
@@ -33,7 +34,7 @@
         @endif
 
         <!-- 本文ここまで -->
-        </div>
     </main>
+</div>
 </body>
 </html>
